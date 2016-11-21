9e1da92c6f403f6482f19960a2edb06548a3ff78
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2016-11-22 00:27:28 +0100

    [ADD] First draft of source files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+ * time estimation v0.1
+ * A tool to calculate the estimated time to complete your tasks
+ *
+ * Copyright (c) 2016 - Tom Lutzenberger (lutzenbergerthomas at gmail dot com)
+ * https://github.com/tomlutzenberger/time-estimation
+ *
+ * Released under the MIT license
+ * https://github.com/tomlutzenberger/time-estimation/LICENSE
+ * https://tomlutzenberger.github.io/time-estimation/
+ */
+
+/*globals $,document*/
+
+$(document).ready(function () {
+
+  'use strict';
+
+});
