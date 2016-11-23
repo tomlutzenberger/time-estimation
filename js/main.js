467f87cc13ca4de22342d4c5239728a5981a3e5d
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2016-11-24 00:10:18 +0100

    Attempt to parse values

--- js/main.js
+++ js/main.js
@@ -10,10 +10,51 @@
  * https://tomlutzenberger.github.io/time-estimation/
  */
 
-/*globals $,document*/
+/*globals $,document,console*/
+
+function isNumeric(value) {
+
+  'use strict';
+
+  return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+
+
+function isValidValue(value) {
+
+  'use strict';
+
+  var isValid = false;
+
+  if (isNumeric(value) && value > 0) {
+    isValid = true;
+
+  } else {
+    console.error('Given value "' + value + '" is not a valid number.');
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+
 
 $(document).ready(function () {
 
   'use strict';
 
+  $('.te').on('change', function () {
+    var
+      neutral = ($('.te-n').val() - 0),
+      optimistic = ($('.te-o').val() - 0),
+      pessimistic = ($('.te-p').val() - 0),
+      result = null;
+
+    if (isValidValue(optimistic) && isValidValue(neutral) && isValidValue(pessimistic)) {
+      result = (optimistic + (4 * neutral) + pessimistic) / 6;
+    }
+
+    $('.result').text(result);
+  });
 });
