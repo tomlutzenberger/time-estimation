6a22bc53923e5f212b491461b68a8c429b77b559
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2016-11-30 00:20:19 +0100

    [RELEASE] First release of Time Estimation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-/*
- * time estimation v0.1
- * A tool to calculate the estimated time to complete your tasks
+/*!
+ * Time Estimation v0.1
+ * Calculate your tasks with PERT
  *
  * Copyright (c) 2016 - Tom Lutzenberger (lutzenbergerthomas at gmail dot com)
  * https://github.com/tomlutzenberger/time-estimation
@@ -10,10 +10,63 @@
  * https://tomlutzenberger.github.io/time-estimation/
  */
 
-/*globals $,document*/
+/*globals $,document,console*/
+
+function isValidValue(value) {
+
+  'use strict';
+
+  var isValid = false;
+
+  if (value > 0) {
+    isValid = true;
+
+  } else {
+    console.error('Given value "' + value + '" is not a valid number.');
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+
+
+function isFormComplete() {
+
+  'use strict';
+
+  if ($('.te-n').val() !== '' && $('.te-o').val() !== '' && $('.te-p').val() !== '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 
 $(document).ready(function () {
 
   'use strict';
 
+  $('.te').on('change', function () {
+    if (isFormComplete()) {
+      var
+        neutral = parseFloat($('.te-n').val()),
+        optimistic = parseFloat($('.te-o').val()),
+        pessimistic = parseFloat($('.te-p').val()),
+        resultMu = null,
+        resultSigma = null;
+
+      if (isValidValue(optimistic) && isValidValue(neutral) && isValidValue(pessimistic)) {
+        resultMu = (optimistic + (4 * neutral) + pessimistic) / 6;
+        resultSigma = (pessimistic - optimistic) / 6;
+      }
+
+      $('.result-mu').text(resultMu.toFixed(2));
+      $('.result-sigma').text(resultSigma.toFixed(2));
+    } else {
+      $('.result-mu').text('');
+      $('.result-sigma').text('');
+    }
+  });
 });
