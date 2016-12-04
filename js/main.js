4db47e43305a54cc2294667b76183f162a2859b0
Tom Lutzenberger <lutzenbergerthomas@gmail.com>
2016-12-04 02:09:40 +0100

    [REFACTOR] Switching to OOP

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*!
- * Time Estimation v0.3
+ * Time Estimation v0.4
  * Calculate your tasks with PERT
  *
  * Copyright (c) 2016 - Tom Lutzenberger (lutzenbergerthomas at gmail dot com)
@@ -12,129 +12,168 @@
 
 /*globals $,document,console*/
 
-function isValidValue(value) {
+function TimeEstimation() {
 
     'use strict';
 
-    var isValid = false;
-
-    if (value > 0) {
-        isValid = true;
-
-    } else {
-        console.warn('Given value "' + value + '" is not a valid number.');
-        isValid = false;
-    }
-
-    return isValid;
+    this.n = null;
+    this.o = null;
+    this.p = null;
+    this.title = null;
+    this.resultMu = null;
+    this.resultSigma = null;
 }
 
 
 
-function isFormComplete() {
+TimeEstimation.prototype = {
 
-    'use strict';
-
-    if ($('.te-n').val() !== '' && $('.te-o').val() !== '' && $('.te-p').val() !== '') {
-        return true;
-    } else {
-        return false;
-    }
-}
+    constructor: TimeEstimation,
 
 
 
-function getValues() {
+    isValidValue: function (value) {
 
-    'use strict';
+        'use strict';
 
-    return {
-        title: $('.te-title').val(),
-        n: parseFloat($('.te-n').val()),
-        o: parseFloat($('.te-o').val()),
-        p: parseFloat($('.te-p').val())
-    };
-}
+        var isValid = false;
 
-
-
-function updateUrlParams() {
-
-    'use strict';
-
-    var
-        values = getValues(),
-        urlParams = '?title=' + encodeURIComponent(values.title) + '&o=' + values.o + '&n=' + values.n + '&p=' + values.p;
-
-    history.pushState(null, null, urlParams);
-}
-
-
-
-function getUrlParams() {
-
-    'use strict';
-
-    var
-        regex = new RegExp('\\?(.*)', 'i'),
-        urlParams = regex.exec(location.search),
-        params = [],
-        p = null,
-        param = null;
-
-    if (urlParams !== null && urlParams.length > 1) {
-        params = urlParams[1].split('&');
-
-        for (p in params) {
-            if (params.hasOwnProperty(p)) {
-                param = params[p].split('=');
-
-                $('.te-' + param[0].toLowerCase()).val(decodeURIComponent(param[1]));
-            }
+        if (value !== '' && value > 0) {
+            isValid = true;
+        } else {
+            console.warn('Given value "' + value + '" is not a valid number.');
+            isValid = false;
         }
 
-        return true;
+        return isValid;
+    },
 
-    } else {
 
-        console.info('No URL parameters to read');
-        return false;
 
+    isFormComplete: function () {
+
+        'use strict';
+
+        if (this.isValidValue(this.o) && this.isValidValue(this.n) && this.isValidValue(this.p)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+
+
+    getValues: function () {
+
+        'use strict';
+
+        this.title = $('.te-title').val();
+        this.n = parseFloat($('.te-n').val());
+        this.o = parseFloat($('.te-o').val());
+        this.p = parseFloat($('.te-p').val());
+    },
+
+
+
+    setValues: function () {
+
+        'use strict';
+
+        $('.te-title').val(this.title);
+        $('.te-n').val(this.n);
+        $('.te-o').val(this.o);
+        $('.te-p').val(this.p);
+    },
+
+
+
+    parseUrlParams: function () {
+
+        'use strict';
+
+        var
+            urlPattern = new RegExp('\\?(.*)', 'i'),
+            urlParamString = urlPattern.exec(location.search);
+
+        if (urlParamString !== null && urlParamString.length > 1) {
+            return urlParamString[1].split('&');
+        } else {
+            return false;
+        }
+    },
+
+
+
+    getUrlParams: function () {
+
+        'use strict';
+
+        var
+            params = this.parseUrlParams(),
+            p = null,
+            param = null;
+
+        if (params !== false) {
+            for (p in params) {
+                if (params.hasOwnProperty(p)) {
+
+                    param = params[p].split('=');
+                    this[param[0].toLowerCase()] = decodeURIComponent(param[1]);
+                }
+            }
+
+            this.setValues();
+
+            return true;
+
+        } else {
+            console.info('No URL parameters to read');
+            return false;
+        }
+    },
+
+
+
+    setUrlParams: function () {
+
+        'use strict';
+
+        var
+            paramString = '?title=' + encodeURIComponent(this.title) + '&o=' + this.o + '&n=' + this.n + '&p=' + this.p;
+
+        history.pushState(null, null, paramString);
+    },
+
+
+
+    calculateResult: function () {
+
+        'use strict';
+
+        this.resultMu = (this.o + (4 * this.n) + this.p) / 6;
+        this.resultSigma = (this.p - this.o) / 6;
+    },
+
+
+
+    setResult: function () {
+
+        'use strict';
+
+        $('.result-mu').text(this.resultMu.toFixed(2));
+        $('.result-sigma').text(this.resultSigma.toFixed(2));
+    },
+
+
+
+    unsetResult: function () {
+
+        'use strict';
+
+        $('.result-mu').text('');
+        $('.result-sigma').text('');
     }
-}
-
-
-
-function resetResult() {
-
-    'use strict';
-
-    $('.result-mu').text('');
-    $('.result-sigma').text('');
-}
-
-
-
-function calculateResult() {
-
-    'use strict';
-
-    var
-        values = getValues(),
-        resultMu = null,
-        resultSigma = null;
-
-    if (isValidValue(values.o) && isValidValue(values.n) && isValidValue(values.p)) {
-        resultMu = (values.o + (4 * values.n) + values.p) / 6;
-        resultSigma = (values.p - values.o) / 6;
-
-        $('.result-mu').text(resultMu.toFixed(2));
-        $('.result-sigma').text(resultSigma.toFixed(2));
-
-    } else {
-        resetResult();
-    }
-}
+};
 
 
 
@@ -142,18 +181,23 @@ $(document).ready(function () {
 
     'use strict';
 
-    if (getUrlParams()) {
-        calculateResult();
+    var te = new TimeEstimation();
+
+    if (te.getUrlParams()) {
+        te.calculateResult();
     }
 
     $('.te').on('change', function () {
 
-        if (isFormComplete()) {
-            calculateResult();
-            updateUrlParams();
+        te.getValues();
+
+        if (te.isFormComplete()) {
+            te.calculateResult();
+            te.setResult();
+            te.setUrlParams();
 
         } else {
-            resetResult();
+            te.unsetResult();
         }
     });
 });
